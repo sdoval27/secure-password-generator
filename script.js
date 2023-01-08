9957c360356247d3e5f97e6c8ffff47a01729e13
unknown <sashardoval@gmail.com>
2023-01-08 14:53:10 -0800

    completed window prompts and non-password options

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '=', '+', '?'];
 
+//array randomizers
 function randomUpper(){
   return uppercase [Math.floor(Math.random()*uppercase.length)];
 }
@@ -22,48 +23,46 @@ function randomSpecial(){
 }
 
 function generatePassword() {
-//password type prompts
- 
-
-//determine password length
-
-  //var passwordLength = window.prompt ('how long do you want your password to be?');
-
- // 
-}
-
-
-
-// Write password to the #password input
-function writePassword() {
-//determine password length
-var passwordLength = window.prompt('How many characters do you want in your password?');
-
+  var passwordLength = window.prompt('How many characters do you want in your password?');
+  var alphabetSoup = [];
+  
+ //determine password length
   if (passwordLength < 8) {
     window.alert('Your password must be longer than 8 characters!');
   } else if (8 <= passwordLength && passwordLength <= 128 ){
     window.alert('Your password is '+ passwordLength + ' characters long.');
     } else {
     window.alert('The maximum password length is 128 characters, make it shorter!');
-    }
-//determine password elements
-var upperLetters = window.prompt ('Do you want to include uppercase letters?(yes/no)');
-//var lowerLetters = window.prompt ('Do you want to include lowercase letters?(yes/no)');
-//var numerical = window.prompt ('Do you want to include numbers?(yes/no)');
-//var special = window.prompt ('Do you want to include special characters?(yes/no)');
+    };
+ console.log(passwordLength);
 
-  if (upperLetters === "yes") {
-    console.log( Math.floor(Math.random(uppercase+=passwordLength)));
-  } //else if(lowerLetters === "yes") {
-    //return Math.floor(Math.random(uppercase)*passwordLength);
-  //} else if(numerical === "yes") {
-  //  return Math.floor(Math.random(uppercase)*passwordLength);
- // } else if(special === "yes") {
-  //  return  Math.floor(Math.random(uppercase)*passwordLength);
-  //}
+ var upperLetters = window.confirm ('Do you want to include uppercase letters?');
+ var lowerLetters = window.confirm ('Do you want to include lowercase letters?');
+ var numerical = window.confirm ('Do you want to include numbers?');
+ var special = window.confirm ('Do you want to include special characters?');
+
+  if (upperLetters){
+    return alphabetSoup = randomUpper()* passwordLength;
+  }
+  if (lowerLetters){
+    return alphabetSoup = randomLower()* passwordLength;
+  }
+  if (numerical){
+    return alphabetSoup = randomNum()* passwordLength;
+  }
+  if (special){
+    return alphabetSoup = specialCharacters()* passwordLength;
+  }
+  if (!upperLetters && !lowerLetters && !numerical && !special){
+    return 'Well this is awkward. . . no password for you I guess.';
+  }
+
+}
 
 
-  //class code  
+// Write password to the #password input
+function writePassword() {
+//determine password elements 
   var password = generatePassword();
   console.log(password);
   var passwordText = document.querySelector("#password");
@@ -71,7 +70,6 @@ var upperLetters = window.prompt ('Do you want to include uppercase letters?(yes
 
 
    passwordText.value = password;
-  //class code ends
 }
 
 // Add event listener to generate button
